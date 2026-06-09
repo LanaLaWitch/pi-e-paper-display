014e5abd4de5e6836d13d2961959cd244927696a
Lana Wells <lana_wells23@hotmail.com>
2026-06-09 21:37:08 +0100

    Removing 'last_displayed' functionality from buffer

--- pico_code/display/display_buffer.py
+++ pico_code/display/display_buffer.py
@@ -1,19 +1,13 @@
-import utime
-
 class DisplayBuffer():
 
     def __init__(self):
         self.buffer = []
-        self.last_displayed = None
 
     def add(self, frame, delay):
         self.buffer.insert(0, (frame, delay))
 
     def get_next(self):
         return self.buffer.pop()
-
-    def update_last_displayed(self):
-        self.last_displayed = utime.ticks_ms()
 
     def get_buffer_run_time(self):
         from epaper_213_v4 import PARTIAL_REFRESH_TIME_MS
