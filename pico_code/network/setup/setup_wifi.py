ea12c69fb0e4c85006cf5aec71d4fd2db56c191d
Lana Wells <lana_wells23@hotmail.com>
2026-06-09 14:24:52 +0100

    Restructuring

--- pico_code/network/setup/setup_wifi.py
+++ pico_code/network/setup/setup_wifi.py
@@ -1,11 +1,12 @@
 def setup_wifi():
-    from network_connect import test_connect_to_wifi
-    from wifi_config_handler import save_wifi_config
+
+    from pico_code.network.wifi_connection_handler import test_connect_to_wifi
+    from pico_code.network.wifi_config_handler import save_wifi_config
 
     print('Beginning Wi-Fi setup')
 
     network_ssid = _get_network_ssid()
-    if network_ssid is None:
+    if network_ssid == None:
         return
 
     psswrd = _get_password()
@@ -18,49 +19,52 @@ def setup_wifi():
     if not connection_attempt_result:
         print('Could not connect to network. Exiting...')
         return
-
+    
     print('Tested connection successfully. Saving credentials for future use')
     save_wifi_config(network_ssid, psswrd)
     print('Credentials Saved')
 
-
 def _get_network_ssid():
-    from network_connect import scan_for_networks
+
+    from pico_code.network.wifi_connection_handler import scan_for_networks
 
     print('Scanning for possible networks...')
 
     found_networks = scan_for_networks()
     if len(found_networks) <= 0:
         print('Could not find any networks to connect to. Exiting...')
-        return None
+        return
 
-    network_name = None
+    valid_ssid_input = False
 
-    while network_name is None:
+    while not valid_ssid_input:
         ssid = input('Enter network name or index: ')
 
         try:
             ssid_as_int = int(ssid)
-            if 0 <= ssid_as_int < len(found_networks):
+            if ssid_as_int >= 0 and ssid_as_int < len(found_networks):
                 network_name = found_networks[ssid_as_int][0].decode('utf-8')
+                valid_ssid_input = True # As redundancy
                 break
-        except ValueError:
+        except:
             pass
 
         for found_network in found_networks:
             if found_network[0].decode('utf-8') == ssid:
                 network_name = ssid
+                valid_ssid_input = True # As redundancy
                 break
 
-        if network_name is None:
+        if not valid_ssid_input:
             print('Invalid network selection, please try again.')
 
     return network_name
 
 
 def _get_password():
+    
     psswrd = input('Enter network password: ')
-    return psswrd
 
+    return psswrd
 
 setup_wifi()
