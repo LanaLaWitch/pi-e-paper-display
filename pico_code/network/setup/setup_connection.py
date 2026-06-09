356b61ba30417a5ecb2bc43132de5ffbe5e77095
Lana Wells <lana_wells23@hotmail.com>
2026-06-09 23:02:52 +0100

    Adjusting to only setup socket from IP and port

--- pico_code/network/setup/setup_connection.py
+++ pico_code/network/setup/setup_connection.py
@@ -1,23 +1,19 @@
-def start_server(port):
-    from wifi_config_handler import get_wifi_config
-    from wifi_connection_handler import connect_to_wifi, disconnect_from_wifi
+def start_server_tcp(ip, port):
     from socket_handler import build_socket_tcp
-
-    ssid, psswrd = get_wifi_config()
-
-    try:
-        ip = connect_to_wifi(ssid, psswrd)
-    except:
-        disconnect_from_wifi()
-        print('Could not connect to network')
-        return
-    
-    if ip is None:
-        return
 
     sock = build_socket_tcp(ip, port)
 
     sock.listen(1)
-    print(f'Socket ready, IP: {ip}')
+    print(f'TCP socket ready, IP: {ip}')
+
+    return sock
+
+def start_server_udp(ip, port):
+    from socket_handler import build_socket_udp
+
+    sock = build_socket_udp(ip, port)
+
+    sock.listen(1)
+    print(f'UDP socket ready, IP: {ip}')
 
     return sock
