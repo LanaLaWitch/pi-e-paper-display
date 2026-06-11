aabb9f16920b2c954a1206a236f07699845eb81a
Lana Wells <lana_wells23@hotmail.com>
2026-06-11 19:36:42 +0100

    Adjusting test case to new code refactor

--- pico_code/test/test_display_functionality.py
+++ pico_code/test/test_display_functionality.py
@@ -3,41 +3,36 @@ import sys
 sys.path.append('/display')
 
 from epaper_213_v4 import EPD_2in13_V4_Landscape
-from display_frame import DisplayFrame, TextData, BYTE_ARRAY, TEXT, INSTRUCTIONS
+from display_frame import DisplayFrame
 from display_buffer import DisplayBuffer
-from run_display import display_buffer
+from frame_components import TextComponent
+from run_display import process_display_buffer
 
 async def test_text_landscape():
     epd = EPD_2in13_V4_Landscape()
 
     buffer = DisplayBuffer()
 
-    text_data = [
-        TextData("Hello World", 0, 10),
-        TextData("EPD Test", 0, 30),
-    ]
-
     frame = DisplayFrame(
-        type=TEXT,
-        data=text_data,
+        components=[
+            TextComponent("Hello World", 0, 10),
+            TextComponent("EPD Test", 0, 30),
+        ],
         is_base=True
     )
 
-    text_data_2 = [
-        TextData("Hello World", 0, 10),
-        TextData("EPD Test", 0, 30),
-        TextData("Waga", 0, 50),
-    ]
-
     frame_2 = DisplayFrame(
-        type=TEXT,
-        data=text_data_2,
+        components=[
+            TextComponent("Hello World", 0, 10),
+            TextComponent("EPD Test", 0, 30),
+            TextComponent("Waga", 0, 50),
+        ]
     )
 
     buffer.add(frame, 1000)
     buffer.add(frame_2, 5000)
 
-    await display_buffer(epd, buffer)
+    await process_display_buffer(epd, buffer)
 
     epd.init()
     epd.Clear()
