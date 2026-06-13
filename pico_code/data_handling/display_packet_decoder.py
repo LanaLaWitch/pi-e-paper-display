53ded29e837c3c6744e4b3064611aa92eb92632b
Lana Wells <lana_wells23@hotmail.com>
2026-06-13 16:38:40 +0100

    Also removing match case

--- pico_code/data_handling/display_packet_decoder.py
+++ pico_code/data_handling/display_packet_decoder.py
@@ -74,50 +74,49 @@ def _decode_text_component(data, draw_colour):
 def _decode_instruction_component(data, draw_colour):
     instruction_type = int.from_bytes(data[0:1], 'big')
 
-    match instruction_type:
-        case InstructionComponent.FILL:
-            return InstructionComponent(InstructionComponent.FILL, draw_colour)
-        case InstructionComponent.PIXEL:
-            x = int.from_bytes(data[1:3], 'big')
-            y = int.from_bytes(data[3:5], 'big')
-            return InstructionComponent(InstructionComponent.PIXEL, draw_colour, x=x, y=y)
-        case InstructionComponent.HLINE:
-            x = int.from_bytes(data[1:3], 'big')
-            y = int.from_bytes(data[3:5], 'big')
-            w = int.from_bytes(data[5:7], 'big')
-            return InstructionComponent(InstructionComponent.HLINE, draw_colour, x=x, y=y, w=w)
-        case InstructionComponent.VLINE:
-            x = int.from_bytes(data[1:3], 'big')
-            y = int.from_bytes(data[3:5], 'big')
-            h = int.from_bytes(data[5:7], 'big')
-            return InstructionComponent(InstructionComponent.VLINE, draw_colour, x=x, y=y, h=h)
-        case InstructionComponent.LINE:
-            x1 = int.from_bytes(data[1:3], 'big')
-            y1 = int.from_bytes(data[3:5], 'big')
-            x2 = int.from_bytes(data[5:7], 'big')
-            y2 = int.from_bytes(data[7:9], 'big')
-            return InstructionComponent(InstructionComponent.LINE, draw_colour, x1=x1, y1=y1, x2=x2, y2=y2)
-        case InstructionComponent.RECT_EMPTY | InstructionComponent.RECT_FILLED:
-            x = int.from_bytes(data[1:3], 'big')
-            y = int.from_bytes(data[3:5], 'big')
-            w = int.from_bytes(data[5:7], 'big')
-            h = int.from_bytes(data[7:9], 'big')
-            filled = instruction_type == InstructionComponent.RECT_FILLED
-            return InstructionComponent(instruction_type, draw_colour, filled=filled, x=x, y=y, w=w, h=h)
-        case InstructionComponent.ELLIPSE_EMPTY | InstructionComponent.ELLIPSE_FILLED:
-            x = int.from_bytes(data[1:3], 'big')
-            y = int.from_bytes(data[3:5], 'big')
-            xr = int.from_bytes(data[5:7], 'big')
-            yr = int.from_bytes(data[7:9], 'big')
-            bitmask = int.from_bytes(data[9:10], 'big') if len(data) > 9 else None
-            filled = instruction_type == InstructionComponent.ELLIPSE_FILLED
-            return InstructionComponent(instruction_type, draw_colour, filled=filled, bitmask=bitmask, x=x, y=y, xr=xr, yr=yr)
-        case InstructionComponent.POLYGON_EMPTY | InstructionComponent.POLYGON_FILLED:
-            x = int.from_bytes(data[1:3], 'big')
-            y = int.from_bytes(data[3:5], 'big')
-            coords = _decode_coords(data[5:])
-            filled = instruction_type == InstructionComponent.POLYGON_FILLED
-            return InstructionComponent(instruction_type, draw_colour, filled=filled, x=x, y=y, coords=coords)
+    if instruction_type == InstructionComponent.FILL:
+        return InstructionComponent(InstructionComponent.FILL, draw_colour)
+    elif instruction_type == InstructionComponent.PIXEL:
+        x = int.from_bytes(data[1:3], 'big')
+        y = int.from_bytes(data[3:5], 'big')
+        return InstructionComponent(InstructionComponent.PIXEL, draw_colour, x=x, y=y)
+    elif instruction_type == InstructionComponent.HLINE:
+        x = int.from_bytes(data[1:3], 'big')
+        y = int.from_bytes(data[3:5], 'big')
+        w = int.from_bytes(data[5:7], 'big')
+        return InstructionComponent(InstructionComponent.HLINE, draw_colour, x=x, y=y, w=w)
+    elif instruction_type == InstructionComponent.VLINE:
+        x = int.from_bytes(data[1:3], 'big')
+        y = int.from_bytes(data[3:5], 'big')
+        h = int.from_bytes(data[5:7], 'big')
+        return InstructionComponent(InstructionComponent.VLINE, draw_colour, x=x, y=y, h=h)
+    elif instruction_type == InstructionComponent.LINE:
+        x1 = int.from_bytes(data[1:3], 'big')
+        y1 = int.from_bytes(data[3:5], 'big')
+        x2 = int.from_bytes(data[5:7], 'big')
+        y2 = int.from_bytes(data[7:9], 'big')
+        return InstructionComponent(InstructionComponent.LINE, draw_colour, x1=x1, y1=y1, x2=x2, y2=y2)
+    elif instruction_type == InstructionComponent.RECT_EMPTY or instruction_type == InstructionComponent.RECT_FILLED:
+        x = int.from_bytes(data[1:3], 'big')
+        y = int.from_bytes(data[3:5], 'big')
+        w = int.from_bytes(data[5:7], 'big')
+        h = int.from_bytes(data[7:9], 'big')
+        filled = instruction_type == InstructionComponent.RECT_FILLED
+        return InstructionComponent(instruction_type, draw_colour, filled=filled, x=x, y=y, w=w, h=h)
+    elif instruction_type == InstructionComponent.ELLIPSE_EMPTY or instruction_type == InstructionComponent.ELLIPSE_FILLED:
+        x = int.from_bytes(data[1:3], 'big')
+        y = int.from_bytes(data[3:5], 'big')
+        xr = int.from_bytes(data[5:7], 'big')
+        yr = int.from_bytes(data[7:9], 'big')
+        bitmask = int.from_bytes(data[9:10], 'big') if len(data) > 9 else None
+        filled = instruction_type == InstructionComponent.ELLIPSE_FILLED
+        return InstructionComponent(instruction_type, draw_colour, filled=filled, bitmask=bitmask, x=x, y=y, xr=xr, yr=yr)
+    elif instruction_type == InstructionComponent.POLYGON_EMPTY or instruction_type == InstructionComponent.POLYGON_FILLED:
+        x = int.from_bytes(data[1:3], 'big')
+        y = int.from_bytes(data[3:5], 'big')
+        coords = _decode_coords(data[5:])
+        filled = instruction_type == InstructionComponent.POLYGON_FILLED
+        return InstructionComponent(instruction_type, draw_colour, filled=filled, x=x, y=y, coords=coords)
 
 
 def _decode_coords(coords_slice):
